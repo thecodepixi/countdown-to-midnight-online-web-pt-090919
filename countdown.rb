11417f00773f68d29ad10aebbf6aa0e76758553a
Emily Harber <emily.a.harber@gmail.com>
2019-09-12 03:26:38 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  while x > 0 
+    puts "#{x} SECOND(S)!"
+    x -= 1 
+  end 
+  
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(x)
+   while x > 0 
+    puts "#{x} SECOND(S)!"
+    x -= 1 
+    sleep(1)
+  end 
+  
+  "HAPPY NEW YEAR!"
+end 
